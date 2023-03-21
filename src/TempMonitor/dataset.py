e0846c505517077a14bbf832079bb9447cb52110
Oscar Jin <35246973+OscarJin@users.noreply.github.com>
2023-03-21 23:36:03 +0800

    update dataset.py

--- src/TempMonitor/dataset.py
+++ src/TempMonitor/dataset.py
@@ -13,6 +13,7 @@ from torch_geometric.data import Data
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Line3DCollection
 
+
 class GraphDataset(InMemoryDataset):
     r"""The ModelNet10/40 datasets from the `"3D ShapeNets: A Deep
     Representation for Volumetric Shapes"
@@ -58,7 +59,8 @@ class GraphDataset(InMemoryDataset):
 
     @property
     def raw_file_names(self):
-        return ['vtu']### define later
+        return ['vtu']  # define later
+        pass
 
     @property
     def processed_file_names(self):
@@ -67,13 +69,13 @@ class GraphDataset(InMemoryDataset):
     def process(self):
         dx = 0.002
         base_depth = 50e-3
-            
+
         data_list = []
-        paths = glob.glob(osp.join(self.raw_dir,'vtu', f'*.vtu'))            
+        paths = glob.glob(osp.join(self.raw_dir, 'vtu', f'*.vtu'))
         for path in paths:
-            node_features, edge_index = self.cell2graph(path,dx,base_depth)
+            node_features, edge_index = self.cell2graph(path, dx, base_depth)
             print("Processed file: {}".format(path))
-            self.plot_graph(node_features[:,:3],edge_index)
+            self.plot_graph(node_features[:, :3], edge_index)
             data = Data(x=node_features, edge_index=edge_index)
             data_list.append(data)
 
@@ -87,52 +89,53 @@ class GraphDataset(InMemoryDataset):
 
     def __repr__(self):
         return '{}{}({})'.format(self.__class__.__name__, self.name, len(self))
-        
-    def cell2graph(self, path,dx,base_depth):
+
+    def cell2graph(self, path, dx, base_depth):
         mesh = meshio.read(path)
-        
+
         points = mesh.points
         points /= 1e3
         points = torch.from_numpy(points.astype(np.float32))
-        
+
         sol_center = torch.tensor(mesh.cell_data['T'][0].astype(np.float32))
-        sol_center = torch.unsqueeze(sol_center,dim=1)
-        
+        sol_center = torch.unsqueeze(sol_center, dim=1)
+
         cells = mesh.cells_dict['hexahedron'].astype(int)
-        
-        ### the cells with non-zero temperature are activated cells
-        centeroids = torch.mean(points[cells],dim=1)        
-        active_mask = (sol_center[:,0] > 0.1) & (centeroids[:,2]>0) ### remove base
+
+        # the cells with non-zero temperature are activated cells
+        centeroids = torch.mean(points[cells], dim=1)
+        active_mask = (sol_center[:, 0] > 0.1) & (centeroids[:, 2] > 0)  # remove base
         sol_center = sol_center[active_mask]
-        
-        cells = cells[active_mask] 
 
-        centeroids = centeroids[active_mask]         
+        cells = cells[active_mask]
 
-        ### convert centeroid to inds (float coordinates to integer coordicate)       
+        centeroids = centeroids[active_mask]
+
+        # convert centeroid to inds (float coordinates to integer coordicate)
         inds = self._coord2inds(centeroids, dx)
-        max_inds = torch.max(inds,dim=0).values
-        inds_hash = self._grid_hash(inds,max_inds)
-        lookup_table = self._create_lookup_table(inds_hash,max_inds)
-        
-        origins = torch.arange(inds.shape[0],dtype=torch.int32)
-        moves = torch.tensor([[1,0,0],
-                          [0,1,0],
-                          [0,0,1]],dtype=torch.int32)
+        max_inds = torch.max(inds, dim=0).values
+        inds_hash = self._grid_hash(inds, max_inds)
+        lookup_table = self._create_lookup_table(inds_hash, max_inds)
+
+        origins = torch.arange(inds.shape[0], dtype=torch.int32)
+        moves = torch.tensor([[1, 0, 0],
+                              [0, 1, 0],
+                              [0, 0, 1]], dtype=torch.int32)
         edges = []
         for i_move in range(3):
             moved_inds = inds + moves[i_move]
-            mask_in = (moved_inds[:,0]>=0)&(moved_inds[:,0]<=max_inds[0])&(moved_inds[:,1]>=0)&(moved_inds[:,1]<=max_inds[1])&\
-                        (moved_inds[:,2]>=0)&(moved_inds[:,2]<=max_inds[2])
-            moved_inds_hash = self._grid_hash(moved_inds[mask_in],max_inds)           
+            mask_in = (moved_inds[:, 0] >= 0) & (moved_inds[:, 0] <= max_inds[0]) & (moved_inds[:, 1] >= 0) & (
+                        moved_inds[:, 1] <= max_inds[1]) & \
+                      (moved_inds[:, 2] >= 0) & (moved_inds[:, 2] <= max_inds[2])
+            moved_inds_hash = self._grid_hash(moved_inds[mask_in], max_inds)
             end0 = origins[mask_in]
             end1 = lookup_table[moved_inds_hash]
-            mask_act = end1>=0
-            edg =  torch.stack([end0[mask_act],end1[mask_act]],dim=0)
-            print(edg.shape)            
+            mask_act = end1 >= 0
+            edg = torch.stack([end0[mask_act], end1[mask_act]], dim=0)
+            print(edg.shape)
             edges.append(edg)
-        edges_total = torch.cat(edges,dim=1)
-        node_features = torch.cat((centeroids,sol_center),dim=1)
+        edges_total = torch.cat(edges, dim=1)
+        node_features = torch.cat((centeroids, sol_center), dim=1)
         return node_features, edges_total
 
     def mesh2graph(self, path):
@@ -151,7 +154,7 @@ class GraphDataset(InMemoryDataset):
 
         # Create edge index tensor from faces
         faces = torch.tensor(faces, dtype=torch.int32).t().contiguous()
-        edges = torch.cat([torch.stack([face, torch.roll(face, -1, 0)], dim=1) for face in faces]) ### duplicate edges?
+        edges = torch.cat([torch.stack([face, torch.roll(face, -1, 0)], dim=1) for face in faces])  # duplicate edges?
         edges = edges.transpose(0, 1).contiguous()
 
         node_features = torch.tensor(vertices, dtype=torch.float)
@@ -162,31 +165,31 @@ class GraphDataset(InMemoryDataset):
     def find_boundary_points(self, data):
         ####
         pass
-        
+
     def _coord2inds(self, points, d):
-        coord_min = torch.min(points,dim=0, keepdim=True).values
-        inds = torch.round((points-coord_min)/d).int()
+        coord_min = torch.min(points, dim=0, keepdim=True).values
+        inds = torch.round((points - coord_min) / d).int()
         return inds
-        
-    def _grid_hash(self,arr,max_inds):
-        int_hash = arr[:,0]+arr[:,1]*(max_inds[0]+1) +arr[:,2]*(max_inds[0]+1)*(max_inds[1]+1)
+
+    def _grid_hash(self, arr, max_inds):
+        int_hash = arr[:, 0] + arr[:, 1] * (max_inds[0] + 1) + arr[:, 2] * (max_inds[0] + 1) * (max_inds[1] + 1)
         return int_hash.long()
-        
-    def _create_lookup_table(self,arr_hash,max_inds):
-        lookup_table = -1*torch.ones(((max_inds[0]+1)*(max_inds[1]+1)*(max_inds[2]+1)),dtype=torch.int64)
+
+    def _create_lookup_table(self, arr_hash, max_inds):
+        lookup_table = -1 * torch.ones(((max_inds[0] + 1) * (max_inds[1] + 1) * (max_inds[2] + 1)), dtype=torch.int64)
         lookup_table[arr_hash] = torch.arange(arr_hash.shape[0])
         return lookup_table
-        
-    def plot_graph(self, points,edge_index):
+
+    def plot_graph(self, points, edge_index):
         points = points.numpy()
         edge_index = edge_index.numpy()
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
-        p1s = points[edge_index[0,:]]
-        ax.plot(p1s[:,0], p1s[:,1], p1s[:,2], '.r', markersize=3)
-        p2s = points[edge_index[1,:]]
+        p1s = points[edge_index[0, :]]
+        ax.plot(p1s[:, 0], p1s[:, 1], p1s[:, 2], '.r', markersize=3)
+        p2s = points[edge_index[1, :]]
         ls = np.hstack([p1s, p2s]).copy()
-        ls = ls.reshape((-1,2,3))
+        ls = ls.reshape((-1, 2, 3))
         lc = Line3DCollection(ls, linewidths=0.5, colors='b')
         ax.add_collection(lc)
         plt.show()
